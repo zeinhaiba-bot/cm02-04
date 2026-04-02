8b220ddf8f0aa6d1310d6fa25d5e159ca2b80ad6
VeilV <zeinhaiba@gmail.com>
2026-04-02 19:10:28 +0300

    Add files via upload

---
+++ ОЛ.py
@@ -0,0 +1,7 @@
+def get_middle_point(x1, y1, x2, y2):
+    x_middle = (x1 + x2) / 2
+    y_middle = (y1 + y2) / 2
+    return x_middle, y_middle
+
+print(get_middle_point(0, 0, 10, 0))
+print(get_middle_point(1, 5, 8, 3))
